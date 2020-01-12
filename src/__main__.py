edabe3a7d36f4a5ef1290eafefaaca20c390caa6
AshBlade <jeenamurali100tab@gmail.com>
2020-01-12 14:55:28 +0530

    Layout: added crossword grid, MAX_GRID, etc

--- src/__main__.py
+++ src/__main__.py
@@ -1,24 +1,38 @@
-#!/usr/bin/python
+# !/usr/bin/python
+# BUG: The pause on minimize feature seems to take too much cpu during idle
+# NOTE: The loading of the grid uses kivy clock, not multi-threading (fix it)
+# NOTE: Suppress the logging after the game is finished to improve performance
+# NOTE:     import os; os.environ["KIVY_NO_CONSOLELOG"] = '1'
+
+import gc
 import sys
 import kivy
 import ctypes
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.config import Config
+from kivy.base import EventLoop
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
-from src.common import LOG, RES
+from src.common import LOG, RES, MAX_GRID
 
 kivy.require('1.11.1')
 
 # Setting up the configuration of the game
 Config.set('kivy', 'log_dir', LOG)
 Config.set('kivy', 'exit_on_escape', True)
-Config.set('kivy', 'pause_on_minimize', True)
+Config.set('kivy', 'pause_on_minimize', False)
 Config.set('kivy', 'allow_screensaver', False)
 Config.set('kivy', 'window_icon', RES + 'win.png')
 
 Config.write()
+
+
+class WordButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 class MainLayout(Widget):
@@ -28,6 +42,23 @@ class MainLayout(Widget):
 class WordJam(App):
     def build(self):
         return MainLayout()
+
+    def on_start(self):
+        Clock.schedule_once(self.async_grid, 1)
+        EventLoop.window.bind(on_keyboard=self.event_keyboard)
+
+    def on_pause(self):
+        gc.collect()
+        return True
+
+    def async_grid(self, x):
+        for i in range(MAX_GRID):
+            self.root.ids.grid.add_widget(WordButton(text='X'))
+        return True
+
+    def event_keyboard(self, window, key, *largs):
+        if key == 27:
+            sys.exit(0)
 
 
 if __name__ == "__main__":
--- src/common.py
+++ src/common.py
@@ -1,5 +1,10 @@
+# !/usr/bin/python
+# The common global constant for the game
+
 import os
 
 PATH = os.getcwd()
 LOG = PATH + '/logs/'
 RES = PATH + '/res/'
+FONT_COLOR = 0.5, 0.5, 0.5, 1
+MAX_GRID = 255
