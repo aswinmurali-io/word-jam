c0dc878f879a95ced41e3f0a728c72916ce305df
AshBlade <jeenamurali100tab@gmail.com>
2020-01-12 11:12:35 +0530

    Setup the basic layout

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+import sys
 import kivy
+import ctypes
 
 from kivy.app import App
 from kivy.config import Config
@@ -11,9 +13,9 @@ kivy.require('1.11.1')
 
 # Setting up the configuration of the game
 Config.set('kivy', 'log_dir', LOG)
-Config.set('kivy', 'allow_screensaver', False)
 Config.set('kivy', 'exit_on_escape', True)
 Config.set('kivy', 'pause_on_minimize', True)
+Config.set('kivy', 'allow_screensaver', False)
 Config.set('kivy', 'window_icon', RES + 'win.png')
 
 Config.write()
@@ -29,4 +31,8 @@ class WordJam(App):
 
 
 if __name__ == "__main__":
+    # Fix blurry font because text scalling issue in windows
+    if 'win32' in sys.platform:
+        ctypes.windll.shcore.SetProcessDpiAwareness(1)
+    # Entry point
     WordJam().run()
