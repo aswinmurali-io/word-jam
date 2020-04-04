69a3ecad312bc98eb1fb194c481c3ba4170c47f4
aswinmurali-io <aswinmurali.co@gmail.com>
2020-04-04 08:11:55 +0530

    fix poetry binary path

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,20 +12,21 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
             # python -m pip install pygments docutils pillow --user
             brew install sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
             curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
-            poetry remove kivy
-            poetry install
-            poetry add kivy
-            poetry run python -m nuitka --mingw64 --include-plugin-directory=nuitka-dependencies.py --standalone main.py
-            poetry run python nuitka-optimise.py
+            $HOME/.poetry/bin/poetry remove kivy
+            $HOME/.poetry/bin/poetry install
+            $HOME/.poetry/bin/poetry add kivy
+            $HOME/.poetry/bin/poetry run python -m nuitka --mingw64 --include-plugin-directory=nuitka-dependencies.py --standalone main.py
+            $HOME/.poetry/bin/poetry run python nuitka-optimise.py
             ;;
     esac
 else
     # Install some custom requirements on Linux
+    sudo apt-get update
     sudo apt-get install python3-setuptools python3-opengl python3-dev mesa-common-dev build-essential python3-pip libgl1-mesa-dev libgles2-mesa-dev zlib1g-dev
     curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
-    poetry remove kivy
-    poetry install
-    poetry add kivy
-    poetry run python -m nuitka --mingw64 --include-plugin-directory=nuitka-dependencies.py --standalone main.py
-    poetry run python nuitka-optimise.py
+    $HOME/.poetry/bin/poetry remove kivy
+    $HOME/.poetry/bin/poetry install
+    $HOME/.poetry/bin/poetry add kivy
+    $HOME/.poetry/bin/poetry run python -m nuitka --mingw64 --include-plugin-directory=nuitka-dependencies.py --standalone main.py
+    $HOME/.poetry/bin/poetry run python nuitka-optimise.py
 fi
